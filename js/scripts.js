89f4e9c1700157c1370cd6d64c1e3089395372ce
stiva98 <samustiva9810@gmail.com>
2023-06-21 18:26:31 +0200

    ADD cambio colore e stampa in console del click

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 const square = 100;
 const gridContainer = document.querySelector('.container');
 const myButton = document.querySelector('.my-button');
-myButton.addEventListener('click', function(){
+myButton.addEventListener('click', function() {
 
     for (let i = 1; i <= square; i++) {
         const newCell = document.createElement('div');
         newCell.classList.add('cell');
         newCell.append(i);
-    
+
+        newCell.addEventListener('click', function () {
+            newCell.classList.toggle('new-bg');
+            console.log(newCell.innerHTML);
+        })
+     
         gridContainer.append(newCell);
     }
 })
+
