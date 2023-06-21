bc230618a2c6f075b607f9fa64ba5ae2846a257e
stiva98 <samustiva9810@gmail.com>
2023-06-21 19:39:26 +0200

    ADD bonus

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
-const square = 100;
+let square = 100;
 const gridContainer = document.querySelector('.container');
-const myButton = document.querySelector('.my-button');
-myButton.addEventListener('click', function() {
+const myForm = document.querySelector('.my-form');
+myForm.addEventListener('submit', function(event) {
+    event.preventDefault()
     gridContainer.innerHTML = '';
+
+    const mySelect = document.getElementById('difficults').value;
+    //console.log(mySelect)
+    if(mySelect == 'easy'){
+       square = 100; 
+    }else if (mySelect == 'medium'){
+       square = 81
+    }else {
+       square = 49;
+    }
 
     for (let i = 1; i <= square; i++) {
         const newCell = document.createElement('div');
-        newCell.classList.add('cell');
+        if(mySelect == 'easy') {
+            newCell.classList.add('cell');
+        }else if (mySelect == 'medium'){
+            newCell.classList.add('cell-2');
+        }else {
+            newCell.classList.add('cell-3');
+        }
         newCell.append(i);
 
         newCell.addEventListener('click', function () {
